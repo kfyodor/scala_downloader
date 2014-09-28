b995eff923ad2d4f0a4cf579d28d913013d59a4b
Theodore Konukhov <komolov.f@gmail.com>
2014-09-28 17:30:00 +0200

    console output in now better

--- src/main/scala/com/konukhov/downloader/ProgressLogger.scala
+++ src/main/scala/com/konukhov/downloader/ProgressLogger.scala
@@ -2,7 +2,9 @@ package com.konukhov.downloader
 
 trait Progress {
   def logProgress(fileName: String, downloaded: Int, length: Int) {
+    val progress = ((downloaded.toFloat / length) * 100)
     print("\r")
-    print(s"Downloading $fileName: " + s"%1.00f".format((downloaded.toFloat / length) * 100) + "%")
+    print(s"-----> Downloading $fileName: " + s"%1.00f".format(progress) + "%")
+    if (progress == 100) print("\n")
   }
 }
